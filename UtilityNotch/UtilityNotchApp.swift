350092875c40623fce7f1343a305d1c860eabba0
BaranovicBen <benjamin.baranovic@gmail.com>
2026-03-06 16:15:34 +0100

    Initial Commit

---
+++ UtilityNotch/UtilityNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtilityNotchApp.swift
+//  UtilityNotch
+//
+//  Created by Benjamín Baranovič on 06.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct UtilityNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
